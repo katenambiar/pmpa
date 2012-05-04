7b5b07c8c93b49b72b79a754a207e5381bcc2b97
Kate Nambiar <k.z.nambiar@gmail.com>
2012-05-04 15:39:42 +0100

    Biobase eset inheritance + initialize method

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -1,9 +1,35 @@
-#-----------------------------------------------------------------------------------------------------
-#   pepArray class definitions
-#-----------------------------------------------------------------------------------------------------
+library(Biobase) #For testing only - Remove in final package
+library(plyr)
 
-# pepArrayRaw Class Definition
-setClass ("pepArray", 
-          representation (FG = "matrix", BG = "matrix", flags = "matrix", peptideAnnotation = "data.frame", sampleAnnotation = "data.frame")
+# pepArray Class Definition
+setClass ("pepArray",
+          contains = "eSet"
           )
 
+# pepArray Constructor Method
+setMethod ("initialize", "pepArray",
+           function(.Object,
+                    assayData = assayDataNew(fg = fg, bg = bg, flags = flags),
+                    fg = new("matrix"),
+                    bg = new("matrix"),
+                    flags = new("matrix"),
+                    phenoData = new("AnnotatedDataFrame"),
+                    featureData = new("AnnotatedDataFrame"),
+                    experimentData = new("MIAME"),
+                    annotation = character(),
+                    protocolData = new("AnnotatedDataFrame"),
+                    ...
+                    ) {
+             
+             if(!missing(assayData) && any(!missing(fg), !missing(bg), !missing(flags))){
+               warning("Using 'assayData'; ignoring 'fg', 'bg', 'flags'")
+             }
+             callNextMethod(.Object, 
+                            assayData = assayData,
+                            phenoData = phenoData,
+                            featureData = featureData,
+                            experimentData = experimentData,
+                            annotation = annotation,
+                            protocolData = protocolData,
+                            ...)
+           })
