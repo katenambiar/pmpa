3d1317a8ad68fe3f7a3d001734142ea673a0ec79
Kate Nambiar <k.z.nambiar@gmail.com>
2012-05-04 08:07:32 +0100

    Dependency change

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -7,18 +7,3 @@ setClass ("pepArrayRaw",
           representation (FG = "matrix", BG = "matrix", flags = "matrix", peptideAnnotation = "data.frame", sampleAnnotation = "data.frame")
           )
 
-# pepArrayCorr Class Definition
-setClass ("pepArrayCorr", 
-          representation (I = "matrix", flags = "matrix", peptideAnnotation = "data.frame", sampleAnnotation = "data.frame")
-          )
-
-# pepArrayNorm Class Definition
-setClass ("pepArrayNorm", 
-          representation (I = "matrix", flags = "matrix", peptideAnnotation = "data.frame", sampleAnnotation = "data.frame")
-          )
-
-# pepArraySummary Class Definition
-setClass ("pepArraySummary", 
-          representation (I = "matrix", flags = "matrix", peptideAnnotation = "data.frame", sampleAnnotation = "data.frame")
-          )
-
