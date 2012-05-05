a1934bd75183af251d7220352e83c30a8083f624
Kate Nambiar <k.z.nambiar@gmail.com>
2012-05-05 09:39:58 +0100

    pepArray inherits from eset - specific accessor methods no longer needed

--- R/Accessor-methods.R
+++
@@ -1,9 +0,0 @@
-#-----------------------------------------------------------------------------------------------------
-#   Accessor Methods
-#-----------------------------------------------------------------------------------------------------
-
-setMethod("getFG", "pepArray", function(x) slot(x, "FG"))
-setMethod("getBG", "pepArray", function(x) slot(x, "BG"))
-setMethod("getFlags", "pepArray", function(x) slot(x, "flags"))
-setMethod("getPepAnnot", "pepArray", function(x) slot(x, "peptideAnnotation"))
-setMethod("getSampAnnot", "pepArray", function(x) slot(x, "sampleAnnotation"))
