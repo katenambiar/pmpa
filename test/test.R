80119c38df807ebb8c33e91b0bcf9050c2c81faf
Kate Nambiar <k.z.nambiar@gmail.com>
2012-05-06 09:22:50 +0100

    update of test files

--- test/test.R
+++ test/test.R
@@ -1,22 +1,14 @@
-# Import target matrix
-targets  <- read.table ("http://scamcompute.hpc.susx.ac.uk/datatrans/kate/LVP/GPR/targets.txt",
-                        stringsAsFactors = FALSE,
-                        header = TRUE,
-                        check.names = FALSE
-                        )
+library(devtools)
+pkg <- as.package("../peparray")
+load_all(pkg)
 
-targets <- targets[2,]
-targets <- t(targets)
-targets <- targets[1:5]
 
-# Define path for GPR directory                            
-gpr.path <- "http://scamcompute.hpc.susx.ac.uk/datatrans/kate/LVP/GPR"
-
-# Define GAL filename
-gal.path <- "http://scamcompute.hpc.susx.ac.uk/datatrans/kate/CDIFF/GAL/JPT_cDiff.gal"
+files <- "C:/Users/Kate/git/peparray/test/fileNames.txt"
+protocol <- "C:/Users/Kate/git/peparray/test/protocolAnnotation.txt"
+sample <- "C:/Users/Kate/git/peparray/test/sampleAnnotation.txt"
 
 # Read GPR Files
-R <- readGPR(targets, gpr.path, gal.path, col = "R")
+R <- readArrays(files, col = "R")
 
 # Get CV of replicates
 R.CV <- arrayCV(R, ndups = 3, spacing = 5184)
