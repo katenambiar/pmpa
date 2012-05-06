56699cb91a97495855414bfba72108a175bd3c18
Kate Nambiar <k.z.nambiar@gmail.com>
2012-05-06 13:08:20 +0100

    prData no longer needed

--- R/AllGenerics.R
+++ R/AllGenerics.R
@@ -2,14 +2,6 @@
 #   Generic methods for peparray
 #-----------------------------------------------------------------------------------------------------
 
-# Protocol data accessor method
-if(!isGeneric("prData")) {
-  setGeneric(
-    name = "prData", 
-    def = function(x, ...) standardGeneric("prData")
-    )
-}
-
 
 # Read Annotation files for peparray objects
 if(!isGeneric("readAnnotation")) {
