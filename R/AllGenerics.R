6536a5a729fe92f3025fd8adc1e5588c378dd225
Kate Nambiar <k.z.nambiar@gmail.com>
2012-05-06 09:22:12 +0100

    readAnnotation generic added

--- R/AllGenerics.R
+++ R/AllGenerics.R
@@ -2,17 +2,33 @@
 #   Generic methods for peparray
 #-----------------------------------------------------------------------------------------------------
 
+# Read Annotation files for peparray objects
+if(!isGeneric("readAnnotation")) {
+  setGeneric(
+    name = "readAnnotation", 
+    def = function(x, ...) standardGeneric("readAnnotation")
+    )
+}
+
+
 # Calculate the CV of intra-array replicates
 if(!isGeneric("arrayCV")) {
   setGeneric(
     name = "arrayCV", 
-    def = function(x, ...) standardGeneric("arrayCV"),
+    def = function(x, ndups, spacing) standardGeneric("arrayCV"),
     useAsDefault = function(x, ndups, spacing){
       x <- as.matrix(x)
       dim(x) <- c(spacing, ndups, ncol(x))
       cv <- apply(x, c(1,3), sd) / apply(x, c(1,3), mean)
       return(cv)
-    }
+      }
     )
-}
+  }
 
+# Calculate the average of intra-array replicates
+if(!isGeneric("arrayAve")) {
+  setGeneric(
+    name = "arrayAve", 
+    def = function(x, ndups, spacing) standardGeneric("arrayAve")
+    )
+  }
