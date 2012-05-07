5d92cb6fabb13ee07a02ee50f7cdd8a31583419d
Kate Nambiar <k.z.nambiar@gmail.com>
2012-05-07 18:43:24 +0100

    fData annotation corrections

--- R/arrayAve-methods.R
+++ R/arrayAve-methods.R
@@ -14,14 +14,15 @@ setMethod(
     if (!is.null(fData(x)$ID)){
       ID <- fData(x)$ID
       ID <- factor(ID, levels = unique(ID))
-    }
-    
+      
     } else {
       stop("Peptide IDs not defined")
+      
     }
   
-    y <- assayData(x)$fg
-    y[assayData(x)flags == 0] <- NA
+    y <- assayDataElement(x, "fg")
+    flags <- assayDataElement(x, "flags")
+    y[flags == 0] <- NA
     sum.y <- rowsum(y, ID, reorder = FALSE, na.rm = TRUE)
     n <- rowsum(1L - is.na(y), ID, reorder = FALSE)
     ave <- sum.y/n
@@ -30,7 +31,8 @@ setMethod(
     assayData(obj) <- assayDataNew(exprs = ave)
     phenoData(obj) <- phenoData(x)
     fData(obj) <- fData(x)[!duplicated(fData(x)$ID), ]
-    fData(obj) <- fData(obj)[ ,!(names(fData(obj)) %in% c("Block", "Column", "Row"))                     
+    featureNames(obj) <- fData(obj)$ID
+    fData(obj) <- fData(obj)[ ,!(names(fData(obj)) %in% c("ID", "Block", "Column", "Row")), drop = FALSE]                     
     experimentData(obj) <- experimentData(x)
     protocolData(obj) <- protocolData(x)
                              
