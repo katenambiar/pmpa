8dc35ce1a58db1ba8ed95f729a6f533f26c61080
Kate Nambiar <k.z.nambiar@gmail.com>
2012-05-08 09:01:45 +0100

    Comments update

--- R/arrayCV-methods.R
+++ R/arrayCV-methods.R
@@ -1,24 +1,27 @@
 #-----------------------------------------------------------------------------------------------------
 #   ArrayCV Methods
 #-----------------------------------------------------------------------------------------------------
-
+#
+#
+# Calculate the CV of intra-array replicates
+# Dependencies: 
+# Arguments: x = PepArrayRaw object or matrix of intensity values with probes as rows and samples in columns
+#            ndups = number of technical replicates on the microarray
+#            spacing = number of rows separating a probe from its replicate
+# Output:    matrix of CV values with samples in columns and unique probes in rows
+#
+# Kate Nambiar
+# Last Updated: 30.4.2012
 setMethod(
   f = "arrayCV",
   signature = "pepArrayPP",
   definition = function(x, ndups, spacing){
-    # Calculate the CV of intra-array replicates
-    # Dependencies: 
-    # Arguments: x = PepArrayRaw object or matrix of intensity values with probes as rows and samples in columns
-    #            ndups = number of technical replicates on the microarray
-    #            spacing = number of rows separating a probe from its replicate
-    # Output:    matrix of CV values with samples in columns and unique probes in rows
-    #
-    # Kate Nambiar
-    # Last Updated: 30.4.2012
+
     y <- log2(assayData(x)$fg)
     y[assayData(x)$flags == 0] <- NA
     dim(y) <- c(spacing, ndups, ncol(y))
     cv <- apply(y, c(1,3), sd) / apply(y, c(1,3), mean)
     return(cv)
+    
   }
   )
