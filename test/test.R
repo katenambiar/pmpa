b738ed5e7f26347d7f55b1d008d4a271cbfeadca
Kate Nambiar <k.z.nambiar@gmail.com>
2012-05-10 01:14:52 +0100

    file paths changed

--- test/test.R
+++ test/test.R
@@ -6,9 +6,9 @@ load_all(pkg)
 # install_github("peparray", username = "katenambiar")
 
 
-files <- "C:/Users/Kate/git/peparray/test/fileNames.txt"
-protocol <- "C:/Users/Kate/git/peparray/test/protocolAnnotation.txt"
-pheno <- "C:/Users/Kate/git/peparray/test/sampleAnnotation.txt"
+files <- "../peparray/test/fileNames.txt"
+protocol <- "../peparray/test/protocolAnnotation.txt"
+pheno <- "../peparray/test/sampleAnnotation.txt"
 
 # Read GPR Files
 R <- readArrays(files, col = "R")
@@ -22,13 +22,9 @@ R.CV <- arrayCV(R, ndups = 3, spacing = 5184)
 # BG correction
 R.BG <- arrayBGcorr(R, method = "none")
 
-<<<<<<< HEAD
 # IntraArray Norm
 controlSeq <- c("MYPIYNTPDNLWFGY", "SRRYVPNQLTKVRLQ", "GHYSWIAKAVLQGEG")
 R.Norm <- intraArrayNorm(R.BG, controlSeq, method = "lm")
-=======
-# IA norm
->>>>>>> e13c3a7281ad08ab8c435d52456cfa0b52be0900
 
 # Average intraArray Replicates
 R.ave <- arrayAve(R.Norm)
