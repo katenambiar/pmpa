49dccc8da394d165d0d697076ad25600dbae9202
Kate Nambiar <k.z.nambiar@gmail.com>
2012-05-14 08:03:50 +0100

    ArraySecab method added

--- R/arraySecAb-methods.R
+++ R/arraySecAb-methods.R
@@ -1,5 +1,5 @@
 setMethod(
-  f = "arrayCV",
+  f = "arraySecAb",
   signature = "pepArrayPP",
   definition = function(x, secabID, transform = "none"){
   
@@ -26,4 +26,35 @@ setMethod(
     assayDataElement(z, "fg") <- normdata
     return (z)
     }
+  )
+
+
+setMethod(
+  f = "arraySecAb",
+  signature = "pepArray",
+  definition = function(x, secabID, transform = "none"){
+    
+    if (transform == "none"){
+      secabdata <- assayDataElement(x[ , sampleNames(x) == secabID], "exprs")
+      arraydata <- assayDataElement(x[ , sampleNames(x) != secabID], "exprs")
+      
+    } else {
+      transformExpression <- parse(text = paste(transform, "(y)", sep = ""))
+      transformFunc <- function (y){
+        eval(transformExpression)
+      }
+      
+      secabdata <- assayDataElement(x[ , sampleNames(x) == secabID], "exprs")
+      secabdata <- transformFunc(secabdata)
+      arraydata <- assayDataElement(x[ , sampleNames(x) != secabID], "exprs")
+      arraydata <- transformFunc(arraydata)
+    }
+    
+    fitlm <- apply(arraydata, 2, function (y) lm(y ~ secabdata))
+    normdata <- sapply(fitlm, function(y) y$resid)
+    
+    z <- x[ , sampleNames(x) != secabID]
+    assayDataElement(z, "exprs") <- normdata
+    return (z)
+  }
   )
