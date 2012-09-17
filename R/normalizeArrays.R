1618c94dda3660b7533e33e682ec34d388253d34
Kate Nambiar <kate@localhost.(none)>
2012-09-17 13:04:27 +0100

    Scale norm function added

--- R/normalizeArrays.R
+++ R/normalizeArrays.R
@@ -10,37 +10,39 @@ setMethod(
     if (method == "none"){
       
       return(x)
+    
+    } else if(method == "scale"){
+      
+      ndata <- aply(x, 2, median, na.rm = TRUE)
+      ndata <- ndata - exp(mean(log(ndata)))
+      assayDataElement(x, "fMedian") <- sweep(fg(x), 2, ndata)
+      return(x)
       
     } else if(method == "quantile"){
       
       assayDataElement(x, "fMedian") <- normalize.quantiles(fg(x))
       return(x)
+    
+    } else if(method == "cyclic"){
       
-      } else if(method == "scale"){
+      assayDataElement(x, "fMedian") <- normalizeCyclicLoess(fg(x), method = "affy")
+      return(x)
         
-        assayDataElement(x, "fMedian") <- normalizeBetweenArrays(fg(x), method = "scale")
-        return(x)
+    } else if (method == "LM"){
       
-      } else if(method == "cyclic"){
+      ndata <- fg(x)
+      ndata <- as.numeric(ndata)
+      ndata <- data.frame(Intensity = ndata, 
+                          ID = rep(sampleNames(x), each = nrow(x)), 
+                          SA = rep(fData(x)$Subarray, ncol(x))
+                          )
+      fit <- lm(Intensity ~ as.factor(ID) + as.factor(ID):as.factor(SA), data = ndata)
+      normdata <- resid(fit) + coef(fit)[1]
+      normdata <- matrix(normdata, ncol = ncol(x))
+      assayDataElement(x, "fMedian") <- normdata
+      return (x)
         
-        assayDataElement(x, "fMedian") <- normalizeCyclicLoess(fg(x), method = "affy")
-        return(x)
-        
-      } else if (method == "LM"){
-        
-        ndata <- fg(x)
-        ndata <- as.numeric(ndata)
-        ndata <- data.frame(Intensity = ndata, 
-                            ID = rep(sampleNames(x), each = nrow(x)), 
-                            SA = rep(fData(x)$Subarray, ncol(x))
-                            )
-        fit <- lm(Intensity ~ as.factor(ID) + as.factor(ID):as.factor(SA), data = ndata)
-        normdata <- resid(fit) + coef(fit)[1]
-        normdata <- matrix(normdata, ncol = ncol(x))
-        assayDataElement(x, "fMedian") <- normdata
-        return (x)
-        
-      } 
+    } 
     
       stop("Method must be either 'scale', 'quantile', 'cyclic', 'LM' or 'none'")
     
