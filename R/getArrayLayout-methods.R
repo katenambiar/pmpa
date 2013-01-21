fde63e2f82c7de5776312819d7cbf5e9eeb1d747
katenambiar <k.z.nambiar@bsms.ac.uk>
2013-01-21 22:40:05 +0000

    Multiset dispatch method

--- R/getArrayLayout-methods.R
+++ R/getArrayLayout-methods.R
@@ -1,17 +1,19 @@
 setMethod(
   f = "getArrayLayout",
-  signature = "eSet",
+  signature = "MultiSet",
   definition = function(x, ncols = 4, subarrays = 3){
     
     if(!all(c("Block", "Row", "Column") %in% names(fData(x))))
       stop("Block, Row and Column indexing not defined for the arrays")
     
-    block.row <- ((fData(x)$Block - 1) %/% ncols) + 1
-    block.col <- ((fData(x)$Block - 1) %% ncols) + 1
-    spot.row <- fData(x)$Row
-    spot.col <- fData(x)$Column
-    subarray <- ((block.row - 1) %/% (max(block.row)/subarrays)) + 1
-    
+    y <- data.matrix(data.frame(
+      block.row = ((fData(x)$Block - 1) %/% ncols) + 1,
+      block.col = ((fData(x)$Block - 1) %% ncols) + 1,
+      spot.row = fData(x)$Row,
+      spot.col = fData(x)$Column,
+      subarray = ((block.row - 1) %/% (max(block.row)/subarrays)) + 1
+    )) 
+    return (y)
   }
     
 )
