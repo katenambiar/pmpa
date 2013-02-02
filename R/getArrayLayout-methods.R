d3a06b03e5455db0e453ea18202f7664701baab0
katenambiar <k.z.nambiar@bsms.ac.uk>
2013-02-02 01:28:56 +0000

    arrayLayout update

--- R/getArrayLayout-methods.R
+++ R/getArrayLayout-methods.R
@@ -4,15 +4,16 @@ setMethod(
   definition = function(x, ncols = 4, subarrays = 3){
     
     if(!all(c("Block", "Row", "Column") %in% names(fData(x))))
-      stop("Block, Row and Column indexing not defined for the arrays")
+      stop("Feature data does not contain Block, Row and/or Column indexes")
     
-    y <- data.matrix(data.frame(
-      block.row = ((fData(x)$Block - 1) %/% ncols) + 1,
-      block.col = ((fData(x)$Block - 1) %% ncols) + 1,
-      spot.row = fData(x)$Row,
-      spot.col = fData(x)$Column,
-      subarray = ((block.row - 1) %/% (max(block.row)/subarrays)) + 1
-    )) 
+    block.row <- ((fData(x)$Block - 1) %/% ncols) + 1
+    block.col <- ((fData(x)$Block - 1) %% ncols) + 1
+    spot.row <- fData(x)$Row
+    spot.col <- fData(x)$Column
+    subarray <- ((block.row - 1) %/% (max(block.row)/subarrays)) + 1
+    
+    y <- cbind(block.row, block.col, spot.row, spot.col, subarray)  
+    
     return (y)
   }
     
