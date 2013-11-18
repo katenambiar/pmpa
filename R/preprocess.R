e512f6f3df4460a6010f55f438f547191c6fd3a8
Kate Nambiar <k.z.nambiar@gmail.com>
2013-11-18 01:14:37 +0000

    ROxygen documentation added

--- R/preprocess.R
+++ R/preprocess.R
@@ -1,7 +1,18 @@
-preprocess <- function (rawdata, controlID, bg.method = "subtract", norm.method, summary.method, ...){
-  secdata <- arraySecFilter(rawdata, control.arrays = controlID, plot = FALSE)
+#' Preprocess peptide microarray data
+#' 
+#' \code{preprocess} is a wrapper function that executes the 4 pre-processing functions 
+#' (arraySecFilter, arrayBGcorr, arayNorm, arraySummary) within the pippa package.
+#' 
+#' @param rawdata
+#' @param sec.filter
+#' @return an object of class ExpressionSet
+#' @export
+preprocess <- function (rawdata, sec.filter = TRUE, bg.method = "subtract", norm.method, summary.method, ...){
+  if (sec.filter == TRUE){
+    secdata <- arraySecFilter(rawdata, plot = FALSE, ...)
+  }
   bgcorrdata <- arrayBGcorr(secdata, method = bg.method, ...)
-  normdata <- arrayNorm(bgcorrdata, method = norm.method)
-  summarydata <- arraySummary(normdata, method = summary.method)
+  normdata <- arrayNorm(bgcorrdata, method = norm.method, ...)
+  summarydata <- arraySummary(normdata, method = summary.method, ...)
   return (summarydata)
 } 
