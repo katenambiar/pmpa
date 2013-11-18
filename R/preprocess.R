5fbeaaf836eab3f2f51686cae9f63f69e1fc4a82
Kate Nambiar <k.z.nambiar@gmail.com>
2013-11-18 00:37:36 +0000

    1st commit

---
+++ R/preprocess.R
@@ -0,0 +1,7 @@
+preprocess <- function (rawdata, controlID, bg.method = "subtract", norm.method, summary.method, ...){
+  secdata <- arraySecFilter(rawdata, control.arrays = controlID, plot = FALSE)
+  bgcorrdata <- arrayBGcorr(secdata, method = bg.method, offset = offset)
+  normdata <- arrayNorm(bgcorrdata, method = norm.method)
+  summarydata <- arraySummary(normdata.method = summary.method)
+  return (summarydata)
+} 
