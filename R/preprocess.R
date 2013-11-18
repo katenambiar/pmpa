9791f5f4339ef98635166ae78c72ceaaf35db0f6
Kate Nambiar <k.z.nambiar@gmail.com>
2013-11-18 00:54:18 +0000

    ... added to bgcorrection

--- R/preprocess.R
+++ R/preprocess.R
@@ -1,6 +1,6 @@
 preprocess <- function (rawdata, controlID, bg.method = "subtract", norm.method, summary.method, ...){
   secdata <- arraySecFilter(rawdata, control.arrays = controlID, plot = FALSE)
-  bgcorrdata <- arrayBGcorr(secdata, method = bg.method, offset = offset)
+  bgcorrdata <- arrayBGcorr(secdata, method = bg.method, ...)
   normdata <- arrayNorm(bgcorrdata, method = norm.method)
   summarydata <- arraySummary(normdata.method = summary.method)
   return (summarydata)
