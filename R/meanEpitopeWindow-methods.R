1400aaa8c9e563eecd2d1ebeb15f5405611cb4d6
Kate Nambiar <k.z.nambiar@gmail.com>
2013-12-06 20:26:18 +0000

    matrix method added for meanEpitopeWindow

--- R/meanEpitopeWindow-methods.R
+++ R/meanEpitopeWindow-methods.R
@@ -33,5 +33,33 @@ setMethod(
   }
 )
 
-
+setMethod(
+  f = "meanEpitopeWindow",
+  signature = "matrix",
+  definition = function(arraydata, epitope.length, protein.sequence) {
+    
+    peptide <- rownames(arraydata)
+    
+    window.seqs <- sapply(seq(1,nchar(protein.sequence),1), function(i) substr(protein.sequence, i, i + epitope.length - 1))
+    window.seqs <- window.seqs[-which(nchar(window.seqs) < epitope.length)]
+    
+    index <- sapply(window.seqs, function(x) grep(x, peptide), simplify = FALSE)
+    window.intensity <- lapply(index, function(x) arraydata[x, ])
+    
+    window.mean.intensity <- lapply(window.intensity, function(x){
+      if(is.matrix(x)){
+        y <-  colMeans(x)
+        return(y)
+      } else{
+        return(x)
+      }
+    }
+    )
+    
+    window.mean.intensity <- do.call(rbind.data.frame, window.mean.intensity)
+    colnames(window.mean.intensity) <- colnames(arraydata)
+    
+    return(window.mean.intensity)
+  }
+)
 
