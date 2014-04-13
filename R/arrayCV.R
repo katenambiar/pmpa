fc5aef79aad7cc89ffc12780b6cf78e451b382d4
katenambiar <k.z.nambiar@gmail.com>
2014-04-13 22:42:08 +0100

    can use MultiSet object directly

--- R/arrayCV.R
+++ R/arrayCV.R
@@ -4,10 +4,15 @@
 #' @param ID vector of probe IDs
 #' @return matrix of CV values with samples in columns and unique probes in rows
 #' @export
-arrayCV <- function(x, ID){
-  sum.x <- rowsum(x, ID, reorder = FALSE, na.rm = TRUE)
-  sumsq.x <- rowsum(x^2, ID, reorder = FALSE, na.rm = TRUE)
-  n <- rowsum(1L - is.na(x), ID, reorder = FALSE)
+arrayCV <- function(x){
+  intensity <- fg(x)
+  ID <- fData(x)$ID
+  flagged.values <- flags(x) == -100
+  intensity[flagged.values] <- NA
+  
+  sum.x <- rowsum(intensity, ID, reorder = FALSE, na.rm = TRUE)
+  sumsq.x <- rowsum(intensity^2, ID, reorder = FALSE, na.rm = TRUE)
+  n <- rowsum(1L - is.na(intensity), ID, reorder = FALSE)
   ave <- sum.x/n
   var.x <- ((n * sumsq.x) - sum.x^2)/n^2
   cv <- sqrt(var.x) / ave
