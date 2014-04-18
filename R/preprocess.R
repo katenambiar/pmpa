f3732b05bd7e61bec465f0493d9957d6711b053a
Kate Nambiar <k.z.nambiar@gmail.com>
2014-04-18 20:45:14 +0100

    updated for amended arrayCV function

--- R/preprocess.R
+++ R/preprocess.R
@@ -13,7 +13,7 @@ preprocess <- function (rawdata, sec.filter = TRUE, control.arrays = NULL, remov
   }
   bgcorrdata <- arrayBGcorr(secdata, method = bg.method, offset = bg.offset)
   normdata <- arrayNorm(bgcorrdata, method = norm.method)
-  cv <- arrayCV(fg(normdata), ID = fData(normdata)$ID)
+  cv <- arrayCV(normdata)
   cv20 <- unique(names(unlist(apply(cv, 2, function(x) x[which(x > 0.20)]))))
   summarydata <- arraySummary(normdata, method = summary.method)
   summarydata <- summarydata[!featureNames(summarydata) %in% cv20, ]
