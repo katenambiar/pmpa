d127d1dcc453273280f17dec23a6dd06fdeedb08
Kate Nambiar <k.z.nambiar@gmail.com>
2014-05-05 11:23:43 +0100

    Control probe and GMM scaling

--- R/scaleNorm.R
+++ R/scaleNorm.R
@@ -6,6 +6,43 @@
 scaleNorm <- function(x){
   y <- apply(x, 2, median, na.rm = TRUE)
   y <- y - exp(mean(log(abs(y))))
-  y <- sweep(x, 2, y)
-  return(y)
+  z <- sweep(x, 2, y)
+  return(z)
+}
+
+#' Scale Normalisation To Control Probe
+#' 
+#' @param x matrix of intensity values with probes as rows and samples in columns
+#' @return matrix of normalised intensities
+#' @export
+scaleNormControl <- function(x, controlID){
+  
+  y <- x[rownames(x) %in% controlID, ]
+  
+  if (is.matrix(y)){
+    y <- apply(y, 2, median, na.rm = TRUE)
+  }
+  
+  y <- y - exp(mean(log(abs(y))))
+  z <- sweep(x, 2, y)
+  return(z)
+}
+
+#' Scale Normalisation using GMM
+#' 
+#' @param x matrix of intensity values with probes as rows and samples in columns
+#' @return matrix of normalised intensities
+#' @export
+scaleNormGMM <- function(x){
+  
+  model.gmm <- list()
+  for (i in 1:ncol(x)){
+    set.seed(123)
+    model.gmm[[i]] <- normalmixEM(x[,i], k = 2, maxit = 100, epsilon = 0.001, fast = TRUE)
+  }
+  
+  mean.model.gmm <- sapply(model.gmm, function(x) x$mu[1])
+  y <- mean.model.gmm - exp(mean(log(abs(mean.model.gmm))))
+  z <- sweep(x, 2, y) 
+  return(z)
 }
