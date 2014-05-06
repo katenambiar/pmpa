6c8d7b3956057b0ae80302c9ba97bac1838ed0cf
Kate Nambiar <k.z.nambiar@gmail.com>
2014-05-06 23:16:27 +0100

    max iterations changed to 100

--- R/scaleNorm.R
+++ R/scaleNorm.R
@@ -38,7 +38,7 @@ scaleNormGMM <- function(x){
   model.gmm <- list()
   for (i in 1:ncol(x)){
     set.seed(123)
-    model.gmm[[i]] <- normalmixEM(x[,i], k = 2, maxit = 1000, epsilon = 0.001, fast = TRUE)
+    model.gmm[[i]] <- normalmixEM(x[,i], k = 2, maxit = 100, epsilon = 0.001, fast = TRUE)
   }
   
   mean.model.gmm <- sapply(model.gmm, function(x) x$mu[1])
