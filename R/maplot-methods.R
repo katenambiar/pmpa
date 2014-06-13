04f17f393072685937f1502aec41b162e59f053b
Kate Nambiar <k.z.nambiar@bsms.ac.uk>
2014-06-13 18:04:11 +0100

    Uses M and A slots in ExpressionSet generated by arrayControlMA()

--- R/maplot-methods.R
+++ R/maplot-methods.R
@@ -2,7 +2,7 @@
 #' 
 #' Creates simple MA plot
 #' 
-#' @param x MultiSet or ExpressionSet object
+#' @param x ExpressionSet object
 #' @param arr1 integer corresponding to 1st array to be plotted
 #' @param arr2 integer corresponding to 2nd array to be plotted
 #' @param ... other arguments passed to plot
@@ -16,30 +16,18 @@ setGeneric(
   def = function(x, ...) standardGeneric("maplot")
 )
 
-setMethod(
-  f = "maplot",
-  signature = "MultiSet",
-  definition = function(x, arr1, arr2, ...){
-    y <- log2(fg(x))
-    M <- y[ ,arr1] - y[ ,arr2]
-    A <- (y[ ,arr1] + y[ ,arr2])/2
-    plot(A, M,
-         pch = 20,
-         ...
-         )
-  }
-)
 
 setMethod(
   f = "maplot",
   signature = "ExpressionSet",
-  definition = function(x, arr1, arr2, ...){
-    y <- exprs(x)
-    M <- y[ ,arr1] - y[ ,arr2]
-    A <- (y[ ,arr1] + y[ ,arr2])/2
-    plot(A, M,
-         pch = 20,
-         ...
-    )
+  definition = function(x, arrayID, ...){
+    if (is.element("M", assayDataElementNames(x)) & is.element("A", assayDataElementNames(x))){
+      M <- assayDataElement(x, "M")[ ,arrayID]
+      A <- assayDataElement(x, "A")[ ,arrayID]
+      plot(A, M, ...)
+    }
+    else {
+      stop("M and A elements not found in ExpressionSet.")
+    }
   }
 )
