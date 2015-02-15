59bcf0a54b013dd7fd4ebab43729e070a06bef5e
Kate Nambiar <k.z.nambiar@bsms.ac.uk>
2015-02-15 02:11:47 +0000

    Wavelength specific data header extraction

--- R/readArrayHeader.R
+++ R/readArrayHeader.R
@@ -5,10 +5,11 @@
 #' @param x filename including full path for the array header to be read
 #' @return data frame
 #' @export
-readArrayHeader <- function(x){
+readArrayHeader <- function(x, wavelength, wavelength.field = "Wavelengths"){
   con <- file(x, open = "r")
   on.exit(close(con))
 
+  # Read header lines from GPR file
   i <- 0
   header <- list()
   repeat{
@@ -21,7 +22,31 @@ readArrayHeader <- function(x){
   }
   header[[i]] <- NULL
   
-  header.df <- data.frame(Reduce(rbind, header), row.names = 1)
-
-  return(header.df)
+  # Check ATF header length - warning issued if mismatch (potential corrupted or edited header)
+  if (as.integer(header[[2]][1]) + 2 != length(header)){
+    warning("ATF header length field in", basename(x), "incorrect. Header length of", length(header), "lines used.")
+  }
+  
+  header[[2]] <- c("HeaderLines", length(header))
+  
+  
+  wl.row <- which(sapply(header, function(y) y[1]) == wavelength.field)
+  
+  if (!length(wl.row)){
+    warning("Wavelength field not found - default header values used.")
+    header.data <- sapply(header, function(y) y[2])
+    header.df <- data.frame(header.data, row.names = sapply(header, function(y) y[1]))
+    return (header.df)
+    
+  } else {
+    
+    wl.col <- which(header[[wl.row]] == wavelength)
+    wl.data <- sapply(header, function(y) y[wl.col])
+    wl.data <- wl.data[!is.na(wl.data)]
+    
+    header.data <- sapply(header, function(y) y[2])
+    header.data[!is.na(sapply(header, function(y) y[wl.col]))] <- wl.data
+    header.df <- data.frame(header.data, row.names = sapply(header, function(y) y[1]))
+    return(header.df)    
+  }
 }
