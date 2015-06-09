67deecc9c9a2a7bc7b364ac7b0d0883cb11ed9a0
Kate Nambiar <k.z.nambiar@bsms.ac.uk>
2015-06-09 11:41:10 +0100

    labelling for extreme M values

--- R/maplot-methods.R
+++ R/maplot-methods.R
@@ -20,15 +20,26 @@ setGeneric(
 setMethod(
   f = "maplot",
   signature = "ExpressionSet",
-  definition = function(x, arrayID, ...){
+  definition = function(x, arrayID, labels = FALSE, nlabels = 1, ...){
     if(length(arrayID) != 2){
       stop("arrayID must be an integer vector of length 2 corresponding to the arrays to be plotted.")
     }
     y <- exprs(x)
     M <- y[ ,arrayID[1]] - y[ ,arrayID[2]]
     A <- (y[ ,arrayID[1]] + y[ ,arrayID[2]])/2
-    plot(A, M, ...)
-    lines(lowess(A,M), col = "blue")
+    MAdata <- data.frame(M, A, row.names = featureNames(x))
+    MAdata <- MAdata[order(MAdata$M, decreasing = TRUE), ]
+    
+    plot(M ~ A, data = MAdata, ...)
+    lines(lowess(A,M), col = "green")
     abline(h = 0, col = "red")
+    
+    
+    text(M ~ A, data = MAdata[1:nlabels, ],
+         labels = rownames(MAdata)[1:nlabels], 
+         cex = 0.5, 
+         pos = 4, 
+         col = "red"
+    )
   }
 )
